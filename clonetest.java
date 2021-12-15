8222f86ec2a8a97f8111435ae19afad875d7c5a8
yi-inaba <95971384+yi-inaba@users.noreply.github.com>
2021-12-15 23:43:45 +0900

    Create clonetest.java

---
+++ clonetest.java
@@ -0,0 +1,14 @@
+public class k005 {
+
+	public static void main(String[] args) {
+		// "こんにちは"と"世界"を連結して出力してください
+	    System.out.println("こんにちは"+"世界");
+	    
+	    // "38"と"19"を連結して出力してください
+	    System.out.println("38"+"19");
+	    
+	    // 38と19を足して出力してください
+	    System.out.println(38+19);
+	}
+
+}
